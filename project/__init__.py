56ab587fa0a4db0c76b990e38ae122682bebd507
Tiffany Nguyen <tiffanyknnc@gmail.com>
2019-11-20 15:37:30 -0500

    removing track changes

--- project/__init__.py
+++ project/__init__.py
@@ -13,6 +13,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/tiff/GitHub/kfw-backend/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from .models import TextFile
